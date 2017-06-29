a6fae2f78e86d2a52b67055ff92e2974ebbff751
Patryz <enrico.patrizi@live.it>
2017-06-29 15:32:08 +0200

    asd

---
+++ ex11.py
@@ -0,0 +1,7 @@
+print "How old?",
+age = raw_input()
+print "How tall?",
+height = raw_input()
+print "How much weight?",
+weight = raw_input()
+print "So, you are %r old, %r tall, and you weight %r." % (age, height, weight)
---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "Script's called: ", script
+print "Var 1:", first
+print "Var 2:", second
+print "Var 3:", third
